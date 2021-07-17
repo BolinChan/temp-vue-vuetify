3182d591429178c07fc34018554da5da92b160ef
bolinc <bolinchan2046@gmail.com>
2021-07-17 13:13:14 +0800

    dynamic title

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ module.exports = {
 	transpileDependencies: ["vuetify"],
 	productionSourceMap: false,
 	chainWebpack: config => {
+		config.plugin("html").tap(args => {
+			args[0].title = process.env.VUE_APP_TITLE;
+			return args;
+		});
 		config.plugin("define").tap(args => {
 			args[0]["process.env"].BASE_URL = JSON.stringify(process.env.BASE_URL);
 			return args;
