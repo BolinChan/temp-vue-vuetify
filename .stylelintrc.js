2c295ba27f983cea634cfc66a40d63fdabdc1de5
bolinc <bolinchan2046@gmail.com>
2021-07-18 16:42:29 +0800

    update stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
 	extends: ["stylelint-config-standard", "stylelint-config-sass-guidelines", "stylelint-config-prettier"],
-	plugins: ["stylelint-order", "stylelint-scss"],
-	rules: {},
+	plugins: ["stylelint-order", "stylelint-scss", "stylelint-declaration-block-no-ignored-properties"],
+	rules: {
+		"no-descending-specificity": null,
+		"function-calc-no-invalid": null,
+		"function-url-quotes": "always",
+		"font-family-no-missing-generic-family-keyword": null,
+		"plugin/declaration-block-no-ignored-properties": true,
+		"unit-no-unknown": [true, { ignoreUnits: ["rpx"] }],
+		// "order/properties-order": ["width", "height"],
+	},
 };
