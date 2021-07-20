2b9f56456d1943c05a1d81503a6c91437ada5603
bolinc <bolinchan2046@gmail.com>
2021-07-20 12:04:27 +0800

    fix vue/no-v-model-argument error

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,59 @@
 <template>
 	<v-app>
-		<v-app-bar app color="primary" dark>
-			<div class="d-flex align-center">
-				<v-img
-					alt="Vuetify Logo"
-					class="shrink mr-2"
-					contain
-					src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-					transition="scale-transition"
-					width="40"
-				/>
+		<v-navigation-drawer v-model="drawer" v-model:mini-variant="mini" app permanent>
+			<v-list-item class="px-2">
+				<v-list-item-avatar>
+					<v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+				</v-list-item-avatar>
 
-				<v-img
-					alt="Vuetify Name"
-					class="shrink mt-1 hidden-sm-and-down"
-					contain
-					min-width="100"
-					src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-					width="100"
-				/>
-			</div>
+				<v-list-item-title>John Leider</v-list-item-title>
 
-			<v-spacer></v-spacer>
+				<v-btn icon @click.stop="mini = !mini">
+					<v-icon>mdi-chevron-left</v-icon>
+				</v-btn>
+			</v-list-item>
 
-			<v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-				<span class="mr-2">Latest Release</span>
-				<v-icon>mdi-open-in-new</v-icon>
-			</v-btn>
-		</v-app-bar>
+			<v-divider></v-divider>
+
+			<v-list dense>
+				<v-list-item v-for="item in items" :key="item.title" link>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
+
+					<v-list-item-content>
+						<v-list-item-title>{{ item.title }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-app-bar app></v-app-bar>
 
 		<v-main>
-			<router-view />
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
 		</v-main>
+
+		<!-- <v-footer app></v-footer> -->
+		<!-- <v-bottom-navigation app></v-bottom-navigation> -->
 	</v-app>
 </template>
 
 <script>
 	export default {
 		name: "App",
-		data: () => ({}),
+		data: () => {
+			return {
+				drawer: true,
+				items: [
+					{ title: "Home", icon: "mdi-home-city" },
+					{ title: "My Account", icon: "mdi-account" },
+					{ title: "Users", icon: "mdi-account-group-outline" },
+				],
+				mini: true,
+			};
+		},
 	};
 </script>
